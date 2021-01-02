112cee12eca601311971285c1b5719a4627c6ee8
Lee Hyeonji <lhj5426@naver.com>
2021-01-02 13:34:25 +0900

    set lazy loading, relationship method

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name="orders")
 @Getter @Setter
@@ -16,14 +18,14 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name="member_id") //매핑을 뭘로할거냐임. 즉 외래키
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems=new ArrayList<>();
 
-    @OneToOne //1대1인 경우에는 자주 엑세스 되는쪽에 외래키 놓는걸 선호
+    @OneToOne(fetch = LAZY , cascade = CascadeType.ALL) //1대1인 경우에는 자주 엑세스 되는쪽에 외래키 놓는걸 선호
     @JoinColumn(name="delivery_id") //연관관계의 주인이 되었음
     private Delivery delivery;
 
@@ -31,5 +33,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
