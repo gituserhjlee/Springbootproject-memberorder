e8d7ca10d5be7b00d94cf4b615b5199542d8bd70
Lee Hyeonji <lhj5426@naver.com>
2021-01-02 16:12:16 +0900

    develop member service

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
@@ -9,9 +10,9 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em; //생성자 인젝션 사용한거임
 
     public void save(Member member){
         em.persist(member);
